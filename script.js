de674c6f6adeda6f0ad991d515671b1ac1e0b087
PRAKASH BISWAS <138035531+prakashh069@users.noreply.github.com>
2024-01-05 01:57:39 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.textContent = inputBox.value;
+
+        let span = document.createElement("span");
+        span.textContent = "\u00d7";
+
+        li.appendChild(span);
+        listContainer.appendChild(li);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    const savedData = localStorage.getItem("data") || "";
+    listContainer.innerHTML = savedData;
+}
+
+showTask();
